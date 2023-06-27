89f8ebb6ca5732630306b038f87a8a720a1d0417
DeenO37 <deen.bds@gmail.com>
2023-06-27 13:35:02 +0200

    add retrofit util

--- src/main/java/com/gfa/third_party/contants/RetrofitContants.java
+++ src/main/java/com/gfa/third_party/contants/RetrofitContants.java
@@ -2,5 +2,5 @@ package com.gfa.third_party.contants;
 
 public class RetrofitContants {
 
-    private static final String BASE_URL = "https://jsonplaceholder.typicode.com/";
+    public static final String BASE_URL = "https://jsonplaceholder.typicode.com/";
 }
--- src/main/java/com/gfa/third_party/util/RetrofitUtil.java
+++ src/main/java/com/gfa/third_party/util/RetrofitUtil.java
@@ -1,4 +1,21 @@
 package com.gfa.third_party.util;
 
+import com.gfa.third_party.contants.RetrofitContants;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitUtil {
+
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getRetrofitInstance() {
+
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(RetrofitContants.BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
 }
